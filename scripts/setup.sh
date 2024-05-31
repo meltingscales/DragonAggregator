9aee92b19c8fcb7f0193f91474fb790a2d34a2a8
Henry Post <HenryFBP@gmail.com>
2024-05-31 10:16:03 -0500

    fix setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,6 @@ set -euxo pipefail
 
 echo "Make sure poetry exists..."
 
-which poetry
-# if exit code is nonzero, it is not a command.
-if [ "$?" -eq "1" ]; then
-  python -m pip install poetry
-fi
+which poetry || python -m pip install poetry
 
 poetry install
